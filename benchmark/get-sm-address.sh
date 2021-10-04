acd6fb1256b11b02378f517f97860d64e9192923
wejdene.haouari <haouari.wejdene1@gmail.com>
2021-10-04 20:42:42 +0100

    deploy contract

---
+++ benchmark/get-sm-address.sh
@@ -0,0 +1,2 @@
+
+curl -X POST  http://localhost:8545 --data '{"jsonrpc":"2.0","method":"eth_getTransactionReceipt","params":["0xd05c7623375c07078423311993fc43374258a88535a93d8def6669da7d9ba589"],"id":1}' -H "Content-Type: application/json"
